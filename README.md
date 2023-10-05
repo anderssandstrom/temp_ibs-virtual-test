6f457feda884e6c6a3995b66f51a8ac7a520e2aa
Carlos Doro Neto <carlos.neto@ess.eu>
2023-10-05 10:04:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Runs an ecmc IOC without associated hardware (useful for simulating a motor reco
 ## IOC template
 
 This project was generated with the [E3 cookiecutter IOC template](https://gitlab.esss.lu.se/ics-cookiecutter/cookiecutter-e3-ioc).
-
-This README.md should be updated as part of creation and should add complementary information about the IOC in question (hardware controlled, addresses, etc.).
--- st.cmd
+++ st.cmd
@@ -1,3 +1,10 @@
-require essioc
+epicsEnvSet IOC NeverGonna:GiveYouUp
 
-iocshLoad("$(essioc_DIR)/common_config.iocsh")
+require ecmccfg 8.0.0
+
+iocshLoad "${ecmccfg_DIR}/startup.cmd" "MASTER_ID=-1, NAMING=ESSnaming, ECMC_VER=8.0.2"
+
+iocshLoad "${ecmccfg_DIR}/configureVirtualAxis.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis1.vax"
+iocshLoad "${ecmccfg_DIR}/applyAxisSynchronization.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis1.sax"
+
+iocshLoad "${ecmccfg_DIR}/setAppMode.cmd"
