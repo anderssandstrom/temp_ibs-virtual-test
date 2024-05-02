4da8ace65b06c68e5a986eb53a7ee8a7d6149c0f
Carlos Doro Neto <carlos.neto@ess.eu>
2024-05-02 10:55:05 +0200

    Vrtual axis test

--- README.md
+++
@@ -1,9 +0,0 @@
-# e3-ioc-ecmcvirtual
-
-Runs an ecmc IOC without associated hardware (useful for simulating a motor record axis).
-
----
-
-## IOC template
-
-This project was generated with the [E3 cookiecutter IOC template](https://gitlab.esss.lu.se/ics-cookiecutter/cookiecutter-e3-ioc).
--- st.cmd
+++ st.cmd
@@ -1,4 +1,4 @@
-epicsEnvSet IOC NeverGonna:GiveYouUp
+epicsEnvSet IOC carlosneto
 
 require ecmccfg 8.0.0
 
@@ -6,5 +6,8 @@ iocshLoad "${ecmccfg_DIR}/startup.cmd" "MASTER_ID=-1, NAMING=ESSnaming, ECMC_VER
 
 iocshLoad "${ecmccfg_DIR}/configureVirtualAxis.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis1.vax"
 iocshLoad "${ecmccfg_DIR}/applyAxisSynchronization.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis1.sax"
+
+iocshLoad "${ecmccfg_DIR}/configureVirtualAxis.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis2.vax"
+iocshLoad "${ecmccfg_DIR}/applyAxisSynchronization.cmd" "CONFIG=${E3_CMD_TOP}/cfg/axis2.sax"
 
 iocshLoad "${ecmccfg_DIR}/setAppMode.cmd"
